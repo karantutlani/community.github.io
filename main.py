3037ccca7510057599670e2e94f9244380f7a78f
Vinh Lam <dangvinh1212@gmail.com>
2022-12-04 18:30:48 -0500

    update code

--- main.py
+++ main.py
@@ -1,80 +1,38 @@
-# This is the database file
-# https://docs.google.com/spreadsheets/d/13AU6tbAudnYdcr1Avl0DTbXAaOEW1pSpP1nK8x7Ov2M/edit?usp=sharing
 import pandas as pd
 from yattag import Doc
-from template import start, header, banner, main, footer, scripts
-
-sheet_names = [
-    "WELCOME!",
-    "Sheet37",
-    "Abuse",
-    "Addictions",
-    "AIDS Hepatitis C",
-    "Bereavement",
-    "Anger Management",
-    "Black Lives",
-    "Brain InjuryTumour",
-    "Clothing Donations",
-    "Community Programs",
-    "Disability",
-    "Education",
-    "Emergency Services",
-    "Employment Services",
-    "Family Services",
-    "Food Banks",
-    "Financial",
-    "Francophones",
-    "Government",
-    "Healthcare",
-    "Homelessness",
-    "Housing",
-    "Indigenous People",
-    "LGBTQ2S+",
-    "Legal Assistance",
-    "Mental Health",
-    "Newcomers",
-    "Pregnancy",
-    "Older Adults",
-    "Transportation",
-    "Youth",
-]
+from template import start, header, banner, filter, main, footer, scripts
+from sheet_detail import sheet_detail
+import numpy as np
 
 doc, tag, text = Doc().tagtext()
 
-with tag("h1"):
-    text("Hello world!")
+def generate_html():
+    for sheet_name, sheet_id in sheet_detail.items():
+        try:
+            spreadsheet_id = "13AU6tbAudnYdcr1Avl0DTbXAaOEW1pSpP1nK8x7Ov2M"
+            url = f"https://docs.google.com/spreadsheets/d/{spreadsheet_id}/gviz/tq?tqx=out:csv&gid={sheet_id}"
+            #Drop columns and rows with NaN values
+            df = pd.read_csv(url)
+            #df.drop(columns='1', inplace=True)# remove unnecessary index column called "1"
+            df.dropna(axis= 1,how='all', inplace=True)# remove columns with all NaN values
+            df.dropna(axis= 0,how='all', inplace=True)# remove rows with all NaN values
+            df.drop([0], axis=0, inplace=True) # Drop row title index:0 
+            df.replace(np.nan, "-", inplace=True)
+            with open(f"templates/{sheet_name}.html", "w",encoding="utf-8") as f:
+                f.write(start)
+                f.write(header)
+                f.write(banner(sheet_name.title()))
+                f.write(filter)
+                f.write(pd.DataFrame.to_html(df, header=True, table_id= "myTable"))
+                # put table here from df
+                #f.write(main)
+                # add the generated content
+                #f.write(doc.getvalue())
+                #f.write(footer)
+                f.write(scripts)
+        except:
+            print("Error with {}".format(sheet_name))
+            pass
 
-print(doc.getvalue())
-
-
-for sheet in sheet_names:
-    try:
-        sheet_id = "13AU6tbAudnYdcr1Avl0DTbXAaOEW1pSpP1nK8x7Ov2M"
-        sheet_name = sheet
-        url = f"https://docs.google.com/spreadsheets/d/{sheet_id}/gviz/tq?tqx=out:csv&sheet={sheet_name}"
-        data = pd.read_csv(url)
-        data.dropna(axis=1)
-    except:
-        print("Error with {}".format(sheet))
-        pass
-
-    # print(data.head())
-    try:
-        with open(f"{sheet_name}.html", "w") as f:
-            f.write(start)
-            f.write(header)
-            f.write(banner)
-            f.write(pd.DataFrame.to_html(data))
-            # put table here from df
-            f.write(main)
-            # add the generated content
-            f.write(doc.getvalue())
-            f.write(footer)
-            f.write(scripts)
-
-            # print(pd.DataFrame.to_html(data))
-    except:
-        pass
-
-
-print("Done")
+# Run the function to generate HTML
+generate_html()
